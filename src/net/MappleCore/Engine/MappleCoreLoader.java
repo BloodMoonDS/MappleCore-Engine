198cb8228fa1087d45d88be583a6bcf6cdb379b8
bloodiey <bloodmoondesignx86@gmail.com>
2022-02-14 13:29:59 -0600

    Update 2

---
+++ src/net/MappleCore/Engine/MappleCoreLoader.java
@@ -0,0 +1,8 @@
+package net.MappleCore.Engine;
+import org.ExamplePlugin.TestScript;
+public class MappleCoreLoader {
+	public static void main(String args[]) {
+		TestScript example = new TestScript();
+		example.Onload();
+	}
+}
